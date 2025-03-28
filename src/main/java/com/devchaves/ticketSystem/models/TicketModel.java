2824efe15243f5b613e5334510495f5130a77706
chaves <chavesprogrammer@gmail.com>
2025-03-28 20:23:35 -0300

    add ticket entity

--- src/main/java/com/devchaves/ticketSystem/models/TicketModel.java
+++ src/main/java/com/devchaves/ticketSystem/models/TicketModel.java
@@ -1,4 +1,37 @@
 package com.devchaves.ticketSystem.models;
 
-public class TicketModel {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_tickets")
+public class TicketModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID ticket_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserModel user;
+
+    @Column(nullable = false)
+    private String ticket_title;
+
+    @Column(nullable = false)
+    private String ticket_description;
+
+    @Column(nullable = false)
+    private String ticket_status;
+
+    @Column(nullable = false)
+    private String observation;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+
 }
