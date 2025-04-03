d6b23fe0180a9f44b3de2dbf355445c0da8a604d
chaves <chavesprogrammer@gmail.com>
2025-04-03 18:32:35 -0300

    add findUserByEmail

--- src/main/java/com/devchaves/ticketSystem/repositories/UserRepository.java
+++ src/main/java/com/devchaves/ticketSystem/repositories/UserRepository.java
@@ -3,7 +3,11 @@ package com.devchaves.ticketSystem.repositories;
 import com.devchaves.ticketSystem.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+    Optional<UserModel> findUserByUsersEmail(String email);
+
 }
