309780fb78ab55d314fc1546631ab7fb95b6fdce
chaves <chavesprogrammer@gmail.com>
2025-04-04 22:58:31 -0300

    refatorando a controller/service

--- src/main/java/com/devchaves/ticketSystem/controller/AuthController.java
+++ src/main/java/com/devchaves/ticketSystem/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.devchaves.ticketSystem.DTOS.UserResponseDTO;
 import com.devchaves.ticketSystem.models.UserModel;
 import com.devchaves.ticketSystem.repositories.UserRepository;
 import com.devchaves.ticketSystem.services.TokenService;
+import com.devchaves.ticketSystem.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,28 +21,18 @@ public class AuthController {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final TokenService tokenService;
+    private final UserService userService;
 
-    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder, TokenService tokenService) {
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder, TokenService tokenService, UserService userService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.tokenService = tokenService;
+        this.userService = userService;
     }
 
     @PostMapping("login")
-    public ResponseEntity login(@RequestBody UserCreateDTO userCreateDTO){
-        if(userCreateDTO.getUsersName() == null || userCreateDTO.getUsersPass() == null){
-            return ResponseEntity.badRequest().body("Invalid Credentials");
-        }
-
-        var user = userRepository.findUserByUserName(userCreateDTO.getUsersName()).orElseThrow(() -> new RuntimeException("User) Not Found"));
-
-        if(!passwordEncoder.matches(userCreateDTO.getUsersPass(), user.getUsersPass())){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
-        String token = this.tokenService.generateToken(user);
-
-        return new ResponseEntity(new UserResponseDTO(user.getUsersName(), token), HttpStatus.OK);
-
+    public ResponseEntity login(@RequestBody UserCreateDTO userCreateDTO) {
+        return userService.userLogin(userCreateDTO);
     }
+
 }
--- src/main/java/com/devchaves/ticketSystem/services/UserService.java
+++ src/main/java/com/devchaves/ticketSystem/services/UserService.java
@@ -1,13 +1,42 @@
 package com.devchaves.ticketSystem.services;
 
+import com.devchaves.ticketSystem.DTOS.UserCreateDTO;
+import com.devchaves.ticketSystem.DTOS.UserResponseDTO;
 import com.devchaves.ticketSystem.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
 
-    public UserService(UserRepository userRepository) {
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, TokenService tokenService) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenService = tokenService;
+    }
+
+    public ResponseEntity userLogin(UserCreateDTO userCreateDTO){
+        if(userCreateDTO.getUsersName() == null || userCreateDTO.getUsersPass() == null){
+            return ResponseEntity.badRequest().body("Invalid Credentials");
+        }
+
+        var user = userRepository.findUserByUserName(userCreateDTO.getUsersName()).orElseThrow(() -> new RuntimeException("User Not Found"));
+
+        if(!passwordEncoder.matches(userCreateDTO.getUsersPass(), user.getUsersPass())){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String token = this.tokenService.generateToken(user);
+
+        return new ResponseEntity(new UserResponseDTO(user.getUsersName(), token), HttpStatus.OK);
+
     }
 
 
