6dc8ba20d9e0cf3f757785243b28e4b759541f02
chaves <chavesprogrammer@gmail.com>
2025-04-14 20:35:44 -0300

    open/closed dtos

--- src/main/java/com/devchaves/ticketSystem/DTOS/GenericDTO.java
+++ src/main/java/com/devchaves/ticketSystem/DTOS/GenericDTO.java
@@ -1,4 +1,55 @@
 package com.devchaves.ticketSystem.DTOS;
 
-public class GenericDTO {
+import java.util.List;
+
+public class GenericDTO<T> {
+    private T data;
+    private List<String> errors;
+    private String message;
+    private int statusCode;
+
+    public GenericDTO(T data, List<String> errors, String message, int statusCode) {
+        this.data = data;
+        this.errors = errors;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    public GenericDTO(T data){
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<String> errors) {
+        this.errors = errors;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
 }
+
+
--- src/main/java/com/devchaves/ticketSystem/util/ConverseDTOFilter.java
+++ src/main/java/com/devchaves/ticketSystem/util/ConverseDTOFilter.java
@@ -3,6 +3,6 @@ package com.devchaves.ticketSystem.util;
 import com.devchaves.ticketSystem.DTOS.GenericDTO;
 
 @FunctionalInterface
-public interface ConverseDTO {
+public interface ConverseDTOFilter {
     boolean test(GenericDTO dto);
 }
