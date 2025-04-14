bdac4261b7a742aba7a726e6d1c57bbb70cd77df
chaves <chavesprogrammer@gmail.com>
2025-04-14 20:34:54 -0300

    createUser controller

--- src/main/java/com/devchaves/ticketSystem/controller/AdminController.java
+++ src/main/java/com/devchaves/ticketSystem/controller/AdminController.java
@@ -1,4 +1,30 @@
 package com.devchaves.ticketSystem.controller;
 
+import com.devchaves.ticketSystem.DTOS.UsersDTO.UserCreateDTO;
+import com.devchaves.ticketSystem.services.AdminService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("admin/")
 public class AdminController {
+
+    private final AdminService adminService;
+
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @Operation(summary = "Create user, only for admin with tokens valid")
+    @PostMapping("createUser")
+    public ResponseEntity createUser(@RequestBody @Valid UserCreateDTO userDTO){
+        return adminService.createUser(userDTO);
+    }
+
 }
