7c5d9bff6f4a8c6e687782a5817e1597f4fb9543
chaves <chavesprogrammer@gmail.com>
2025-04-15 23:30:46 -0300

    ConverseDTO generico

--- src/main/java/com/devchaves/ticketSystem/util/converterDTOLogic/ConverseDTO.java
+++ src/main/java/com/devchaves/ticketSystem/util/converterDTOLogic/ConverseDTO.java
@@ -1,4 +1,15 @@
 package com.devchaves.ticketSystem.util.converterDTOLogic;
 
 public class ConverseDTO {
+
+    private final ConverterFactory converterFactory;
+
+    public ConverseDTO(ConverterFactory converterFactory) {
+        this.converterFactory = converterFactory;
+    }
+
+    public <S, T> T convert(S source, Class<T> targetType){
+        return converterFactory.convert(source, targetType);
+    }
+
 }
