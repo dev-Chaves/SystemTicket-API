8e093abe3a01220d63bcb6024940ee3b4951cc7c
chaves <chavesprogrammer@gmail.com>
2025-04-16 14:37:41 -0300

    set  the pattern of converter register

--- src/main/java/com/devchaves/ticketSystem/util/converterDTOLogic/ConverterConfig.java
+++ src/main/java/com/devchaves/ticketSystem/util/converterDTOLogic/ConverterConfig.java
@@ -1,4 +1,28 @@
 package com.devchaves.ticketSystem.util.converterDTOLogic;
 
+import com.devchaves.ticketSystem.DTOS.GenericDTO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class ConverterConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public ConverterConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public ConverterFactory converterFactory(){
+        ConverterFactory factory = new ConverterFactory();
+
+        factory.registerConverter(GenericDTO.class, GenericDTO.class, new GenericDTOConverter());
+
+        return factory;
+
+    }
+
+
 }
