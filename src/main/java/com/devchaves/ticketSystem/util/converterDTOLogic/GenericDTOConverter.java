f6a4c8f26f8d5245667bdcbd65b65e6f3c84baf3
chaves <chavesprogrammer@gmail.com>
2025-04-16 14:37:01 -0300

    making him more generic

--- src/main/java/com/devchaves/ticketSystem/util/converterDTOLogic/GenericDTOConverter.java
+++ src/main/java/com/devchaves/ticketSystem/util/converterDTOLogic/GenericDTOConverter.java
@@ -2,7 +2,7 @@ package com.devchaves.ticketSystem.util.converterDTOLogic;
 
 import com.devchaves.ticketSystem.DTOS.GenericDTO;
 
-public class GenericDTOConverter implements DTOConverter<GenericDTO<?>, GenericDTO<?>> {
+public class GenericDTOConverter<T> implements DTOConverter<GenericDTO<?>, GenericDTO<?>> {
     @Override
     public GenericDTO<?> convert(GenericDTO<?> source) {
         return new GenericDTO<>(source.getData());
