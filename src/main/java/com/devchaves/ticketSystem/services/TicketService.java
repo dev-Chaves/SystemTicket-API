1c65d78eaa52a9378201ff367b61e40fb799d39c
dev-Chaves <chavesprogrammer@gmail.com>
2025-04-21 12:21:38 -0300

    refatorei usando o converterGenerico

--- src/main/java/com/devchaves/ticketSystem/services/TicketService.java
+++ src/main/java/com/devchaves/ticketSystem/services/TicketService.java
@@ -7,6 +7,7 @@ import com.devchaves.ticketSystem.models.UserModel;
 import com.devchaves.ticketSystem.repositories.TicketRepository;
 import com.devchaves.ticketSystem.util.converterDTOLogic.ConverseDTO;
 import com.devchaves.ticketSystem.util.converters.TIcketRequestToModelConverter;
+import com.devchaves.ticketSystem.util.converters.TicketModelToResponseConverter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -41,30 +42,13 @@ public class TicketService {
 
         TicketModel ticket = converter.convert(ticketDTO);
 
-//        TicketModel ticket = new TicketModel();
-//        ticket.setUser(user);
-//        ticket.setTicket_title(ticketDTO.getTicketTitle());
-//        ticket.setTicket_description(ticketDTO.getTicketDescription());
-//        ticket.setTicket_status(ticketDTO.getTicketStatus());
-//        ticket.setObservation(ticketDTO.getObservation());
-//        ticket.setCreatedAt(LocalDateTime.now());
-
         ticketRepository.save(ticket);
 
-        TicketResponseDTO responseDTO = convertToResponseDTO(ticket);
+        TicketModelToResponseConverter converterResponse = new TicketModelToResponseConverter();
 
-        return ResponseEntity.ok().body(responseDTO);
-    }
+        var response = converterResponse.convert(ticket);
 
-    private TicketResponseDTO convertToResponseDTO(TicketModel ticket){
-        return new TicketResponseDTO(
-                ticket.getUser().getUsersName(),
-                ticket.getTicket_title(),
-                ticket.getTicket_description(),
-                ticket.getTicket_status(),
-                ticket.getObservation(),
-                ticket.getCreatedAt()
-        );
+        return ResponseEntity.ok().body(response);
     }
 
 }
