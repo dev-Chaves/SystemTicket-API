43ebf8ced1369c2b81fd8000956fc3da1c189731
dev-Chaves <chavesprogrammer@gmail.com>
2025-04-21 11:40:11 -0300

    dto para respostade usuários padrões

--- src/main/java/com/devchaves/ticketSystem/DTOS/UsersDTO/UserDefaultResponse.java
+++ src/main/java/com/devchaves/ticketSystem/DTOS/UsersDTO/UserDefaultResponse.java
@@ -1,4 +1,24 @@
 package com.devchaves.ticketSystem.DTOS.UsersDTO;
 
 public class UserDefaultResponse {
+
+    private String username;
+    private String userpass;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpass() {
+        return userpass;
+    }
+
+    public void setUserpass(String userpass) {
+        this.userpass = userpass;
+    }
 }
