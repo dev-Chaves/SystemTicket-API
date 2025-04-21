59fcbac3f2154f84e60cf6343324ff9f2b5a6138
dev-Chaves <chavesprogrammer@gmail.com>
2025-04-21 11:41:05 -0300

    conversor para resposta

--- src/main/java/com/devchaves/ticketSystem/util/converters/UserDefaultToResponse.java
+++ src/main/java/com/devchaves/ticketSystem/util/converters/UserDefaultToResponse.java
@@ -1,4 +1,26 @@
 package com.devchaves.ticketSystem.util.converters;
 
-public class UserDefaultToResponse {
+import com.devchaves.ticketSystem.DTOS.UsersDTO.UserDefaultResponse;
+import com.devchaves.ticketSystem.models.UserModel;
+import com.devchaves.ticketSystem.util.converterDTOLogic.DTOConverter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserDefaultToResponse implements DTOConverter<UserModel, UserDefaultResponse> {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public UserDefaultToResponse(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDefaultResponse convert(UserModel source) {
+
+        UserDefaultResponse userDefaultResponse = new UserDefaultResponse();
+
+        userDefaultResponse.setUsername(source.getUsersName());
+        userDefaultResponse.setUserpass(passwordEncoder.encode(source.getUsersPass()));
+
+        return userDefaultResponse;
+    }
 }
